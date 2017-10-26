f4247c764b1ace96fee0c0ac1637565b66d91363
Josua <Josua@LTOP-ACER-JOSUA>
2017-10-26 19:38:39 +0200

    asdf

---
+++ Parkhaus-Digitalisierung/src/priceCalculatorTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class priceCalculatorTest {
+	priceCalculator pC1;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void getPrice_
+}
