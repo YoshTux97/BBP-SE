231793b4a84f6c6635242dd949db47ba7f49c5a1
Josua <Josua@LTOP-ACER-JOSUA>
2017-10-26 19:23:56 +0200

    erster Test

---
+++ Parkhaus-Digitalisierung/src/payTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class payTest {
+	pay pay1;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void paying_
+
+}
