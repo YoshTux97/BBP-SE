961f2eeae5a5cb17afcc1750992b32cd21369b49
Josua <Josua@LTOP-ACER-JOSUA>
2018-01-07 17:35:11 +0100

    asd

--- Parkhaus-Digitalisierung/src/ViewTest.java
+++ Parkhaus-Digitalisierung/src/ViewTest.java
@@ -5,9 +5,22 @@ import org.junit.Test;
 
 public class ViewTest {
 	Parkhaus p;
+	ParkhausModell pm;
+	View v1;
+	View v2;
+	View v3;
 
 	@Before
 	public void setUp() throws Exception {
+		p = new Parkhaus("Test", 10);
+		for (int i = 0; i < 10; ++i) {
+			p.ticketAusstellen();
+		}
+		Ticket[] tick = new Ticket[10];
+		for (int i = 0; i < 10; ++i) {
+			tick[i] = p.getTicket(i);
+		}
+		
 	}
 
 	@Test
