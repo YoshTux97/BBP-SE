9e13517aee52c66662bf756dd89278e3733c71fa
Konsar <ptnntp@yahoo.de>
2018-01-07 17:10:51 +0100

    View2 gecoded

--- Parkhaus-Digitalisierung/src/View2.java
+++ Parkhaus-Digitalisierung/src/View2.java
@@ -1,10 +1,29 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.stream.Stream;
 
 public class View2 implements ViewManager {
-
+	private ParkhausModell parkhausModell;
+	long wocheneinnahmen;
+	
+	public View2(ParkhausModell parkhausModell) {
+		this.parkhausModell = parkhausModell;
+		update();
+	}
+	
 	@Override
 	public void update() {
-		// TODO Automatisch generierter Methodenstub
-
+		wocheneinnahmen = parkhausModell.getTicketsStream()
+				.filter(ticket -> ticket.bezahlt)
+				.filter(ticket -> ticket.ausfahrt != null && Instant.from(getCurrentWochenanfang()).isBefore(ticket.ausfahrt))
+				.mapToLong(ticket -> ticket.preis)
+				.sum();
+	}
+	
+	private LocalDate getCurrentWochenanfang() {
+		LocalDate temp = LocalDate.now();
+		int value = temp.getDayOfWeek().getValue();
+		return temp.minusDays(value - 1);
 	}
 
 }
