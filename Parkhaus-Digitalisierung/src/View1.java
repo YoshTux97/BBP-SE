90293bf65c6f06d66d2b0963efb63e7f73b84472
Josua <Josua@LTOP-ACER-JOSUA>
2018-01-07 16:30:14 +0100

    View1

--- Parkhaus-Digitalisierung/src/View1.java
+++ Parkhaus-Digitalisierung/src/View1.java
@@ -4,18 +4,20 @@ import java.time.LocalDate;
 public class View1 implements ViewManager {
 	private Controller cont;
 	private ParkhausModell modell;
+	public long Tageseinnahmen;
 	
 	public View1(Controller cont, ParkhausModell modell) {
 		this.cont = cont;
 		this.modell = modell;
+		update();
 	}
 
 	@Override
 	public void update() {
-		System.out.println(modell.getTicketsStream()
+		Tageseinnahmen = modell.getTicketsStream()
 				.filter(ticket -> ticket.bezahlt)
 				.filter(ticket -> null != ticket.ausfahrt && Instant.from(LocalDate.now()).isBefore(ticket.ausfahrt))
 				.mapToLong(ticket -> ticket.preis)
-				.sum());
+				.sum();
 	}
 }
