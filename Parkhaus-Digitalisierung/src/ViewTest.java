9c7cbfd94fc201aacfb862bc8c436eb7d4a764d1
Josua <Josua@LTOP-ACER-JOSUA>
2018-01-07 17:30:11 +0100

    Test

--- Parkhaus-Digitalisierung/src/ViewTest.java
+++ Parkhaus-Digitalisierung/src/ViewTest.java
@@ -3,19 +3,11 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ViewManagerTest {
-
+public class ViewTest {
 	Parkhaus p;
-	ParkhausModell pm;
-	ViewManager v1;
-	ViewManager v2;
+
 	@Before
 	public void setUp() throws Exception {
-		p = new Parkhaus("test", 10);
-		for (int i = 0; i < 10; ++i) {
-			p.ticketAusstellen();
-		}
-		
 	}
 
 	@Test
